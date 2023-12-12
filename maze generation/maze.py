62114cfc98fd4e65e5e11fdc4d23ccd5e261155d
khalilDimassi <dimassi0khalil@gmail.com>
2023-12-12 14:40:08 +0100

    initiate maze gen

---
+++ maze generation/maze.py
@@ -0,0 +1,52 @@
+import pygame
+import random
+
+class Maze:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.maze = [[0 for _ in range(height)] for _ in range(width)]
+
+    def generate(self):
+        # Start at a random cell
+        start_x, start_y = random.randint(0, self.width - 1), random.randint(0, self.height - 1)
+        self.maze[start_x][start_y] = 0
+
+        # Keep track of visited cells
+        visited = [[False for _ in range(self.height)] for _ in range(self.width)]
+        visited[start_x][start_y] = True
+
+        # Stack of cells to visit
+        stack = [(start_x, start_y)]
+
+        # Direction vectors for moving in each direction
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        while stack:
+            x, y = stack.pop()
+
+            # Randomly select a direction
+            direction = random.choice(directions)
+
+            # Calculate the new position
+            new_x, new_y = x + direction[0], y + direction[1]
+
+            # Check if the new position is valid and not visited
+            if (0 <= new_x < self.width and 0 <= new_y < self.height and not visited[new_x][new_y]):
+                # Mark the cell as visited and add it to the stack
+                visited[new_x][new_y] = True
+                self.maze[new_x][new_y] = 0
+                stack.append((new_x, new_y))
+
+if __name__ == '__main__':
+    # Initialize Pygame
+    pygame.init()
+
+    # Set up some constants
+    WIDTH = 800
+    HEIGHT = 600
+    FPS = 60
+
+    # Set up the display
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Maze Game")
